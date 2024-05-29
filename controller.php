bca49279c869620059a469db9c64bc649280cfa3
Michele Locati <michele@locati.it>
2024-05-29 13:20:46 +0200

    Version 1.1.0

--- controller.php
+++ controller.php
@@ -22,7 +22,7 @@ class Controller extends Package implements ProviderAggregateInterface
 
     protected $pkgHandle = 'community_store_bcc_payway';
 
-    protected $pkgVersion = '1.0.4';
+    protected $pkgVersion = '1.1.0';
 
     /**
      * {@inheritdoc}
